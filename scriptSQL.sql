317cbb5d81fe235f2d88ecce23c5cc54a34a92ca
Mamednoor <mamed.gohabur@gmail.com>
2021-06-11 17:01:27 +0200

    update three last requests

--- scriptSQL.sql
+++ scriptSQL.sql
@@ -44,23 +44,25 @@ where film_vu.idfilm is null;
 -- Requête qui affiche les films qui ont la meilleure note
 
 select * from film order by note desc;
+SELECT idfilm, titre, note FROM film WHERE note=(SELECT MAX(note) FROM film); 
 
 -- Requête qui donne le film qui a la moins bonne note
 
 select * from film where note=(select min(note) from film); -- donne toutes les informations
 select titre, note from film where note=(select min(note) from film); -- donne juste le titre et la note
 
--- Requêtes qui affichent les films qui ont une plus grande note que la moyenne des notes films
+-- Requête qui affichent les films qui ont une plus grande note que la moyenne des notes films
 
 select * from film where note >(select avg(note) from film);
 
 -- Requête qui pour chaque acteur nous donne le nombre de film où il a joué
 
-select any_value(acteur.nom),any_value(acteur.prenom), count(acteur.idacteur) as nbr_film 
+select any_value(acteur.nom) as nom_acteur ,any_value(acteur.prenom) as prenom_acteur, count(acteur.idacteur) as nbr_film 
 from acteur
 left join film on film.idacteur = acteur.idacteur
 group by acteur.nom 
 order by nbr_film desc;
+
 
 select any_value(acteur.nom) as nom_acteur,any_value(acteur.prenom) as prenom_acteur, count(acteur.idacteur) as nbr_film 
 from acteur
@@ -77,6 +79,19 @@ natural left join film
 group by acteur.nom 
 order by nbr_film desc limit 1;
 
+
+select any_value(acteur.nom) as nom_acteur,any_value(acteur.prenom) as prenom_acteur, count(film.idacteur) as nbr_film 
+from acteur
+natural join film
+group by acteur.idacteur
+having count(film.idacteur) = (
+  select count(idacteur)
+  from film
+  group by idacteur
+  order by count(idacteur) desc
+  limit 1
+);
+
 -- Le nom et prénom et le nombre de film de l’abonné qui a vu le plus de film
 
 select any_value(abonne.nom) as nom_abonne,any_value(abonne.prenom) as prenom_abonne, count(film_vu.idabonne) as nbr_FilmVu
@@ -85,6 +100,19 @@ natural left join film_vu
 group by abonne.nom 
 order by nbr_FilmVu desc limit 1;
 
+
+select any_value(abonne.nom) as nom_abonne,any_value(abonne.prenom) as prenom_abonne, count(film_vu.idabonne) as nbr_FilmVu
+from abonne
+natural left join film_vu 
+group by abonne.nom 
+having count(film_vu.idabonne) = (
+  select count(idabonne)
+  from film_vu
+  group by idabonne
+  order by count(idabonne) desc
+  limit 1
+);
+
 -- Le nom et prénom du réalisateur qui a réalisé le plus de film
 
 select any_value(realisateur.nom), any_value(realisateur.prenom), count(film.idrealisateur) as nbr_de_film
@@ -92,6 +120,19 @@ from realisateur
 natural left join film
 group by realisateur.nom
 order by nbr_de_film desc limit 4;
+
+
+select any_value(realisateur.nom) as nom_realisateur, any_value(realisateur.prenom) as prenom_realisateur, count(film.idrealisateur) as nbr_de_film
+from realisateur
+natural left join film
+group by realisateur.nom
+having count(film.idrealisateur) = (
+  select count(idrealisateur)
+  from film
+  group by idrealisateur
+  order by count(idrealisateur) desc
+  limit 1
+);
 
 -- Créer un trigger, une procédure et un évent de votre choix.
 
